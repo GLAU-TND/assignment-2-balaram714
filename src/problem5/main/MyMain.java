fb84833d531b4cd9e06ea93fdb521f16d30f7cec
balaram714 <62565810+balaram714@users.noreply.github.com>
2020-03-30 19:43:44 +0530

    Update MyMain.java

--- src/problem5/main/MyMain.java
+++ src/problem5/main/MyMain.java
@@ -6,6 +6,41 @@
  */
 package problem5.main;
 //executable class
-import 
+import problem5.student;
+import problem5.circularqueue;
+import java.util.*;
 public class MyMain {
+public static void main(String args[])
+{
+Scanner obj=new Scanner(System.in);
+System.out.println("enter the no of students");
+int b=obj.nextInt();
+ArrayQueue c= new ArrayQueue(b);
+System.out.println("enter the students");
+for(int i=0;i<b;i++)
+{
+System.out.println("enter the name of the students");
+String s=obj.next();
+System.out.println("enter the no of backs");
+int k=obj.nextInt();
+c.offer(new Student(s,k));
+
+}
+Student arr[]=new Student[b];
+for(int j=0;j<b;j++)
+{
+arr[j]=null;
+}
+for(int j=0;j<b;j++)
+{
+Student ab=c.poll();
+if((ab.count!=0)||(ab.count<2))
+arr[j]=ab;
+}
+for(int j=0;j<b;j++)
+{
+if(arr[d]!=null)
+  System.out.println(arr[j].name);
+}
+}
 }
